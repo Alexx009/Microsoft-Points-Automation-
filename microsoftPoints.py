d89e15447ac0178a6dca56afc41c1a764d2e458e
Alexis Desiatco <99580667+Alexx009@users.noreply.github.com>
2023-05-26 19:44:59 +0800

    new api used

--- microsoftPoints.py
+++ microsoftPoints.py
@@ -10,23 +10,24 @@ y = 0
 while(x <= 30):
     x= x + 1
     time.sleep(2)
-    response = requests.get('https://random-word-api.herokuapp.com/word')
+    response = requests.get('https://api.api-ninjas.com/v1/randomword', headers={'X-Api-Key':'7AGWVzVCrnWVPFK7cZEAxQ==yD9utGtt0zTHIlmE'})
     data = response.json()
     pyautogui.hotkey('ctrl', 'e')
-    pyautogui.typewrite(data[0])
+    pyautogui.typewrite(data['word'])
     pyautogui.press('enter')
-    print(x)
+    print(f"{y}: {data['word']}")
 
 
 pyautogui.hotkey('ctrl', 'shift','i')
 
 while(y <= 20):
-    y= y + 1
+    y += 1
     time.sleep(2)
-    response = requests.get('https://random-word-api.herokuapp.com/word')
+    response = requests.get('https://api.api-ninjas.com/v1/randomword', headers={'X-Api-Key':'7AGWVzVCrnWVPFK7cZEAxQ==yD9utGtt0zTHIlmE'})
     data = response.json()
-    
     pyautogui.hotkey('ctrl', 'e')
-    pyautogui.typewrite(data[0])
+    pyautogui.typewrite(data['word'])
     pyautogui.press('enter')
-    print(y)
+    print(f"{y}: {data['word']}")
+
+
