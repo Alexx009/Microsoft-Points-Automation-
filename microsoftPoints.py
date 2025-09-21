035c42bc615c01f85f46c2d5280aae25036a966b
Alexis Dubria Desiatco <99580667+Alexx009@users.noreply.github.com>
2025-09-21 15:28:42 +0800

    Update microsoftPoints.py

--- microsoftPoints.py
+++ microsoftPoints.py
@@ -1,3 +1,4 @@
+
 import pyautogui
 import requests
 import time
@@ -6,9 +7,11 @@ time.sleep(5)
 x = 0
 y = 0
 
+
+
 while x <= 30:
     x += 1
-    time.sleep(4)
+    time.sleep(10)
     response = requests.get('https://api.api-ninjas.com/v1/randomword', headers={'X-Api-Key': '7AGWVzVCrnWVPFK7cZEAxQ==yD9utGtt0zTHIlmE'})
     data = response.json()
     pyautogui.hotkey('ctrl', 'e')
@@ -16,23 +19,11 @@ while x <= 30:
     # Introduce delays between key presses to slow down the typing speed
     for char in data['word']:
         pyautogui.typewrite(char)
-        time.sleep(0.1)  # Adjust the sleep duration as needed
+        time.sleep(10                              )
+        
+          # Adjust the sleep duration as needed
 
     pyautogui.press('enter')
     print(f"{y}: {data['word']}")
 
-pyautogui.press('F12')
-while y <= 20:
-    y += 1
-    time.sleep(4)
-    response = requests.get('https://api.api-ninjas.com/v1/randomword', headers={'X-Api-Key': '7AGWVzVCrnWVPFK7cZEAxQ==yD9utGtt0zTHIlmE'})
-    data = response.json()
-    pyautogui.hotkey('ctrl', 'e')
     
-    # Introduce delays between key presses to slow down the typing speed
-    for char in data['word']:
-        pyautogui.typewrite(char)
-        time.sleep(0.1)  # Adjust the sleep duration as needed
-
-    pyautogui.press('enter')
-    print(f"{y}: {data['word']}")
